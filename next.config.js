28c35352b44b1fa8b3882995b3c4d2682c56ed6a
siddhikreddy <166997192+siddhik-reddy@users.noreply.github.com>
2026-09-08 21:40:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      path: false,
-      crypto: false,
-    };
-    return config;
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
