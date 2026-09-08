1cc5e0876f8db59772a39b045ff6b08c7d462313
siddhikreddy <166997192+siddhik-reddy@users.noreply.github.com>
2026-09-08 21:28:10 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+      crypto: false,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
