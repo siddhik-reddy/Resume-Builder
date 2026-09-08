a6e905db44dd5e51a6370a53602a0b7519fee128
siddhikreddy <166997192+siddhik-reddy@users.noreply.github.com>
2026-09-08 21:39:19 +0530

    Create templates.ts

---
+++ lib/templates.ts
@@ -0,0 +1,28 @@
+export const DEFAULT_TEMPLATE = `\\documentclass{article}
+\\begin{document}
+
+\\begin{center}
+{\\Large \\textbf{Your Name}} \\\\
+\\vspace{2mm}
+Email | Phone | Location
+\\end{center}
+
+\\section*{Experience}
+\\textbf{Job Title} - Company \\\\
+\\textit{Dates} \\\\
+\\begin{itemize}
+  \\item Achievement or responsibility
+  \\item Another achievement
+\\end{itemize}
+
+\\section*{Education}
+\\textbf{Degree} - Institution \\\\
+\\textit{Graduation Year}
+
+\\section*{Skills}
+\\begin{itemize}
+  \\item Skill one
+  \\item Skill two
+\\end{itemize}
+
+\\end{document}`;
